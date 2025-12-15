2b86052ba1bb9fb1b71c5ebc667ab6d83cecdcc9
Jamxon <aliyevjamkhan499@gmail.com>
2025-12-15 21:13:33 +0500

    Initial commit

---
+++ Management.Domain/Models/Student.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Management.Domain.Models;
+
+public class Student
+{
+    public string Id {get; set;}
+
+    public string FirstName {get; set;}
+
+    public string LastName {get; set;}
+}
